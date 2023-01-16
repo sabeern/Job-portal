5a9c02238497fae47822e991d80cb5f49107bd48
sabeern <sabeerneyyan@gmail.com>
2023-01-16 22:48:38 +0530

    Updated employee profile page

--- client/src/containers/employee/EmpProfileDetails.js
+++ client/src/containers/employee/EmpProfileDetails.js
@@ -7,7 +7,8 @@ import FormInputbox from '../../components/FormInputbox';
 function EmpProfileDetails() {
     const userDetails = useSelector((store) => store.allUsers.user);
   return (
-        <Col md={6} className="overflow-auto" style={{maxHeight:'80vh'}}>
+    <>
+        {/* <Col md={6} className="overflow-auto" style={{maxHeight:'80vh'}}>
                 <h1 className='mt-4'>Profile Information</h1>
             <Form>
                 <Row>
@@ -30,7 +31,53 @@ function EmpProfileDetails() {
                     Update Details
                 </Button></Link>
             </Form>
+        </Col> */}
+        <Col md={6} className="overflow-auto mb-4 p-4" style={{maxHeight:'80vh'}}>
+                <h1 className='mt-4'>
+                            Profile Information
+                </h1>
+                <Row>
+                   <Col md={12} className="p-4" style={{border:'1px solid black',borderRadius:'20px'}}>
+                            <table style={{borderRadius:'20px'}}>
+                            <tr>
+                                    <td><b>Full Name</b></td>
+                                    <td>{userDetails.firstName+" "+userDetails.lastName}</td>
+                                </tr>
+                                <tr>
+                                    <td><b>Job Title</b></td>
+                                    <td>{userDetails.jobTitle}</td>
+                                </tr>
+                                <tr>
+                                    <td><b>Qualification</b></td>
+                                    <td>{userDetails.qualification}</td>
+                                </tr>
+                                <tr>
+                                    <td><b>Experience</b></td>
+                                    <td>{userDetails.experience}</td>
+                                </tr>
+                                <tr>
+                                    <td><b>More Details</b></td>
+                                    <td>{userDetails.details}</td>
+                                </tr>
+                                <tr>
+                                    <td>Contact Number</td>
+                                    <td>{userDetails.contactNumber}</td>
+                                </tr>
+                                <tr>
+                                    <td><b>Resume</b></td>
+                                    <td>
+                                        <a href={`http://localhost:8000/resume/${userDetails.resume}`} target="_blank">View Resume</a>
+                                    </td>
+                                </tr>
+                            </table>
+                            <Link to="/empProfile/updateProfile"><Button variant="primary" type="submit">
+                                      Update Details
+                                </Button>
+                            </Link>
+                    </Col> 
+                </Row>      
         </Col>
+        </>
   )
 }
 
